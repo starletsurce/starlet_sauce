d88efdff704d1d07cd746dff3387c6c0f0f98e96
Mini <35373538+42Dugg@users.noreply.github.com>
2021-05-07 16:52:36 +0100

    sunglasses.lua
    https://i.imgur.com/Q1RRQ8V.png
    https://i.imgur.com/CtCQvBl.png

---
+++ sunglasses.lua
@@ -0,0 +1,16 @@
+client.set_event_callback("paint", function()
+
+    local players = entity.get_players(true)
+
+    for i = 1, #players do
+        local player = players[i]
+        local x,y,z = entity.hitbox_position(player,0)
+        local x_world,y_world = renderer.world_to_screen(x,y,z)
+        if not x_world or not y_world then return end
+        renderer.circle(x_world,y_world,255,255,0,255,10,0,1)
+        renderer.rectangle(x_world - 10,y_world - 3,20,3,0,0,0,255) --center
+        renderer.rectangle(x_world - 7,y_world,5,3,0,0,0,255) --left
+        renderer.rectangle(x_world + 3,y_world,5,3,0,0,0,255) --right
+        renderer.rectangle(x_world - 4,y_world + 5,8,3,0,0,0,255) --smile
+    end
+end)
